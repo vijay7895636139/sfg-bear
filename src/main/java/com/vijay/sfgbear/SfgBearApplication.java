88a4a588a52e9730129fff66007d1847fd888ee2
vijay rawat <ajayrawat0207@gmail.com>
2022-07-10 16:30:32 +0530

    first commit

---
+++ src/main/java/com/vijay/sfgbear/SfgBearApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.sfgbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgBearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijay/sfgbear/SfgBearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.sfgbear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgBearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
